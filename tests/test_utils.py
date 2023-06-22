50eba221e12cd51bfb340af08cf432d0ad5e82a4
bdvllrs <bdvllrs@gmail.com>
2023-06-22 13:56:21 +0200

    Fix test_utils.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,7 +2,7 @@ from simple_shapes_dataset.cli.utils import get_deterministic_name
 
 
 def test_get_deterministic_name():
-    domain_alignment = [("v", 0.2), ("t", 0.3)]
+    domain_alignment = {frozenset(["v"]): 0.2, frozenset(["t"]): 0.3}
     seed = 3
     name = get_deterministic_name(domain_alignment, seed)
 
@@ -10,23 +10,15 @@ def test_get_deterministic_name():
 
 
 def test_get_deterministic_name_mutiple_domain():
-    domain_alignment = [("v", 0.2), ("t,v", 0.3)]
+    domain_alignment = {frozenset(["v"]): 0.2, frozenset(["t", "v"]): 0.3}
     seed = 3
     name = get_deterministic_name(domain_alignment, seed)
 
     assert name == "t,v:0.3_v:0.2_seed:3"
 
 
-def test_get_deterministic_name_mutiple_domain_wrong_order():
-    domain_alignment = [("v", 0.2), ("v,t", 0.3)]
+def test_get_deterministic_name_mutiple_domain_different_order():
+    domain_alignment = {frozenset(["v"]): 0.2, frozenset(["v", "t"]): 0.3}
     seed = 3
     name = get_deterministic_name(domain_alignment, seed)
     assert name == "t,v:0.3_v:0.2_seed:3"
-
-
-def test_get_deterministic_name_mutiple_domain_repeated():
-    domain_alignment = [("v", 0.2), ("t,v", 0.3), ("v,t", 0.1)]
-    seed = 3
-    name = get_deterministic_name(domain_alignment, seed)
-
-    assert name == "t,v:0.1_v:0.2_seed:3"
