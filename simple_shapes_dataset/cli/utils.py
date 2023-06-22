Fix computation of group domain split

simple_shapes_dataset/cli/utils.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 from pathlib import Path
-from typing import Dict, Mapping, cast
+from typing import Mapping, cast
 
 import matplotlib.path as mpath
 import numpy as np
@@ -10,6 +10,8 @@ from matplotlib import pyplot as plt
 from torchdata.datapipes.iter import IterableWrapper
 from tqdm import tqdm
 from transformers import BertModel, BertTokenizer
+
+from simple_shapes_dataset.cli.graph import build_dependency_graph
 
 
 @dataclass
@@ -314,15 +316,78 @@ def save_bert_latents(
 
 
 def get_domain_split(
+    seed: int,
     dataset_size: int,
-    aligned_domains_proportion: Mapping[frozenset, float],
-) -> Dict[frozenset, np.ndarray]:
-    print(aligned_domains_proportion)
-    selection = {}
-    for domains, proportion in aligned_domains_proportion.items():
-        assert 0 <= proportion <= 1
-        selection[domains] = np.random.rand(dataset_size) < proportion
-    return selection
+    aligned_domain_groups_proportion: dict[frozenset[str], float],
+) -> dict[frozenset[str], np.ndarray]:
+    aligned_domain_groups_amount = {
+        domain_group: int(
+            dataset_size * aligned_domain_groups_proportion[domain_group]
+        )
+        for domain_group in aligned_domain_groups_proportion
+    }
+
+    rng = np.random.default_rng(seed)
+    rng_streams = {
+        domain_group: stream
+        for domain_group, stream in zip(
+            aligned_domain_groups_proportion.keys(),
+            rng.spawn(len(aligned_domain_groups_proportion)),
+        )
+    }
+
+    selection = {
+        domain_group: (
+            np.array([], dtype=np.int64),
+            rng_stream.permutation(dataset_size),
+        )
+        for domain_group, rng_stream in rng_streams.items()
+    }
+
+    dependency_graph = build_dependency_graph(
+        list(aligned_domain_groups_proportion)
+    )
+
+    while len(dependency_graph.nodes):
+        roots = dependency_graph.get_roots()
+        for root in roots:
+            define_domain_split(
+                selection,
+                aligned_domain_groups_amount,
+                root,
+            )
+        dependency_graph.remove_nodes(roots)
+
+    return {
+        domain_group: np.sort(selected)
+        for domain_group, (selected, _) in selection.items()
+    }
+
+
+def define_domain_split(
+    selection: dict[frozenset[str], tuple[np.ndarray, np.ndarray]],
+    aligned_domain_groups_amounts: dict[frozenset[str], int],
+    domain_group: frozenset[str],
+) -> None:
+    nb_selected = aligned_domain_groups_amounts[domain_group]
+    assert 0 <= nb_selected
+    _, selected = selection[domain_group]
+    selected = selected[:nb_selected]
+
+    for target_domain_group in selection.keys():
+        if target_domain_group <= domain_group:
+            target_selected, target_remaining = selection[target_domain_group]
+            new_selected = np.unique(
+                np.concatenate([target_selected, selected])
+            )
+            nb_added = len(new_selected) - len(target_selected)
+            new_remaining = np.setdiff1d(
+                target_remaining, selected, assume_unique=True
+            )
+            selection[target_domain_group] = (new_selected, new_remaining)
+
+            aligned_domain_groups_amounts[target_domain_group] -= nb_added
+            assert aligned_domain_groups_amounts[target_domain_group] >= 0
 
 
 def get_deterministic_name(

tests/test_utils.py
@@ -1,4 +1,9 @@
-from simple_shapes_dataset.cli.utils import get_deterministic_name
+import numpy as np
+
+from simple_shapes_dataset.cli.utils import (
+    get_deterministic_name,
+    get_domain_split,
+)
 
 
 def test_get_deterministic_name():
@@ -22,3 +27,36 @@ def test_get_deterministic_name_mutiple_domain_different_order():
     seed = 3
     name = get_deterministic_name(domain_alignment, seed)
     assert name == "t,v:0.3_v:0.2_seed:3"
+
+
+def test_get_domain_split():
+    dataset_size = 100
+    domain_groups = get_domain_split(
+        seed=0,
+        dataset_size=dataset_size,
+        aligned_domain_groups_proportion={
+            frozenset(["v"]): 0.8,
+            frozenset(["t"]): 0.9,
+            frozenset(["a"]): 1.0,
+            frozenset(["v", "t"]): 0.4,
+            frozenset(["t", "a"]): 0.2,
+        },
+    )
+
+    for domain_group in domain_groups:
+        assert domain_group in [
+            frozenset(["v"]),
+            frozenset(["t"]),
+            frozenset(["a"]),
+            frozenset(["v", "t"]),
+            frozenset(["t", "a"]),
+        ]
+    assert domain_groups[frozenset(["v"])].shape[0] == 80
+    assert (
+        np.intersect1d(
+            domain_groups[frozenset(["v", "t"])],
+            domain_groups[frozenset(["v"])],
+            assume_unique=True,
+        ).shape[0]
+        == 40
+    )