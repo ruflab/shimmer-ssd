6cc54e7de0cc4917303f5a8236edc067ea0cbab8
bdvllrs <bdvllrs@gmail.com>
2023-06-27 15:26:35 +0200

    Improve __init__ of module folder.

--- simple_shapes_dataset/modules/__init__.py
+++ simple_shapes_dataset/modules/__init__.py
@@ -1,9 +1,13 @@
 from simple_shapes_dataset.modules.dataset import SimpleShapesDataset
 from simple_shapes_dataset.modules.domain import (
+    AVAILABLE_DOMAINS,
+    Attribute,
+    RawText,
     SimpleShapesAttributes,
     SimpleShapesImages,
     SimpleShapesRawText,
     SimpleShapesText,
+    Text,
 )
 from simple_shapes_dataset.modules.domain_alignment import get_aligned_datasets
 
@@ -13,5 +17,9 @@ __all__ = [
     "SimpleShapesText",
     "SimpleShapesRawText",
     "SimpleShapesDataset",
+    "Attribute",
+    "RawText",
+    "Text",
+    "AVAILABLE_DOMAINS",
     "get_aligned_datasets",
 ]
