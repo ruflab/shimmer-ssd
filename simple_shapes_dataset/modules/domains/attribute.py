fix issue with unpaired loss computation for visual.

simple_shapes_dataset/modules/domains/attribute.py
@@ -271,7 +271,7 @@ class AttributeWithUnpairedDomainModule(DomainModule):
             "unpaired": F.mse_loss(
                 pred[:, self.paired_dim :], target[:, self.paired_dim :]
             ),
-            "other": F.mse_loss(
+            "paired": F.mse_loss(
                 pred[:, : self.paired_dim], target[:, : self.paired_dim]
             ),
         }

simple_shapes_dataset/modules/domains/text.py
@@ -5,13 +5,8 @@ import torch
 import torch.nn.functional as F
 from shimmer.modules.domain import DomainModule
 from shimmer.modules.global_workspace import SchedulerArgs
-from shimmer.modules.vae import (
-    VAE,
-    VAEDecoder,
-    VAEEncoder,
-    gaussian_nll,
-    kl_divergence_loss,
-)
+from shimmer.modules.vae import (VAE, VAEDecoder, VAEEncoder, gaussian_nll,
+                                 kl_divergence_loss)
 from torch import nn
 from torch.optim.lr_scheduler import OneCycleLR
 