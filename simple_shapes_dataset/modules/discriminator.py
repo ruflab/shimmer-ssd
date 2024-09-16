406d938875f444af493f35a4aeae8bfdeb35cc94
bdvllrs <bdvllrs@gmail.com>
2024-09-16 08:48:13 +0000

    bigger model

--- simple_shapes_dataset/modules/discriminator.py
+++ simple_shapes_dataset/modules/discriminator.py
@@ -51,6 +51,8 @@ class GWLossesWithDiscriminator(GWLosses):
             nn.ReLU(),
             nn.Linear(hidden_dim, hidden_dim),
             nn.ReLU(),
+            nn.Linear(hidden_dim, hidden_dim),
+            nn.ReLU(),
             nn.Linear(hidden_dim, 1),
         )
 
