c8e78a8e6653184a2dd3af43045e9c021e22c666
bdvllrs <bdvllrs@gmail.com>
2025-01-17 13:35:37 +0000

    fix: missing param config_path in "scripts"

--- scripts/train_gw.py
+++ scripts/train_gw.py
@@ -1,4 +1,5 @@
+from shimmer_ssd import PROJECT_DIR
 from shimmer_ssd.cli.train_gw import train_gw
 
 if __name__ == "__main__":
-    train_gw()
+    train_gw(PROJECT_DIR / "config")
--- scripts/train_t.py
+++ scripts/train_t.py
@@ -1,4 +1,5 @@
+from shimmer_ssd import PROJECT_DIR
 from shimmer_ssd.cli.train_t import train_t_domain
 
 if __name__ == "__main__":
-    train_t_domain()
+    train_t_domain(PROJECT_DIR / "config")
