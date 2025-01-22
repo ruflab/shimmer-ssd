a33a5ed521a26f19d4f67bd96a0da313733ce82a
bdvllrs <bdvllrs@gmail.com>
2025-01-22 11:15:31 +0000

    fix: add ssd train gw to cli

--- shimmer_ssd/cli/cli.py
+++ shimmer_ssd/cli/cli.py
@@ -5,6 +5,7 @@ from shimmer_ssd.cli.download import download_group
 from shimmer_ssd.cli.extract import save_v_latents_command
 from shimmer_ssd.cli.migrate import migrate_domains_command
 from shimmer_ssd.cli.train_attr import train_attr_command
+from shimmer_ssd.cli.train_gw import train_gw_command
 from shimmer_ssd.cli.train_t import train_t_command
 from shimmer_ssd.cli.train_v import train_v_command
 
@@ -27,6 +28,7 @@ def train_group():
 train_group.add_command(train_v_command)
 train_group.add_command(train_attr_command)
 train_group.add_command(train_t_command)
+train_group.add_command(train_gw_command)
 
 
 @cli.group("extract")
