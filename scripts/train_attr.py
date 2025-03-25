eb4a736a28a95afe37674a41ecb68f5baaf4b41c
leopoldmt <leopold.maytie@gmail.com>
2025-03-25 16:28:00 +0000

    change val and test dataloaders to make them closer to train

--- scripts/train_attr.py
+++ scripts/train_attr.py
@@ -1,5 +1,9 @@
 from shimmer_ssd import PROJECT_DIR
 from shimmer_ssd.cli.train_attr import train_attr_domain
 
+import os
+os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"  
+os.environ["CUDA_VISIBLE_DEVICES"]="1"
+
 if __name__ == "__main__":
     train_attr_domain(PROJECT_DIR / "config")
--- scripts/train_gw.py
+++ scripts/train_gw.py
@@ -1,5 +1,9 @@
 from shimmer_ssd import PROJECT_DIR
 from shimmer_ssd.cli.train_gw import train_gw
 
+import os
+os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"  
+os.environ["CUDA_VISIBLE_DEVICES"]="0"
+
 if __name__ == "__main__":
     train_gw(PROJECT_DIR / "config")
